e2fde960b7aa0148809d38d2c4483c01948ab3cd
Admin <Admin@DESKTOP-A2TKCRO>
2020-03-07 19:53:28 +0300

    Create code

--- Solution1/HW.02/Program.cs
+++ Solution1/HW.02/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
-
+using System.IO;
 namespace HW._02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Создаем пространство имен Sustem.IO для файлового ввода и вывода
-            // Класс streamReader (позволит считать сиволы из файла image.txt)
-            // Создаем переменную textReaderResult считываем все символы до конца
-            // Объявляем массив arrayOfTextResult и разделяем внутри цикла символы получившиеся из чтнения файла image.txt пробелами
-            // Объявляем массив imageBytes и внутри цикла конвертируем бинарный код в байты
-            // Цикл For и условие ( пока i<arrayOfTextResult.Length - 1 цикл будет выполняться)
-            // Собираем вместе байты и Выводим изображение в формате /png
-            // прерываем StreamRender
+            StreamReader textReader = new StreamReader(@"D:\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+            File.WriteAllBytes(@"D:\Игры\image.png", imageBytes);
+            textReader.Dispose();
         }
     }
 }
