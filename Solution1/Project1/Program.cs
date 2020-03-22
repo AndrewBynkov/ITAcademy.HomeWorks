53b08b156c6bd0b96074d79aaf61819efe1894c1
Admin <Admin@DESKTOP-A2TKCRO>
2020-03-22 21:56:28 +0300

    HomeWork_05

---
+++ Solution1/Project1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("English alphabet!\n");
+
+            for (char i = '\u005A'; i >= '\u0041'; i--)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Solution1/Project2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Push any button!\n");
+            string pushButton = Console.ReadLine();
+            char symbolButtons = Convert.ToChar(pushButton);
+
+            switch (symbolButtons)
+            {
+                case ('w'):
+                    Console.WriteLine("Move up!");
+                    break;
+                case ('a'):
+                    Console.WriteLine("Move left!"); 
+                    break;
+                case ('d'):
+                    Console.WriteLine("Move right");
+                    break;
+                case ('s'):
+                    Console.WriteLine("Move down");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break; 
+            }
+        }
+    }
+}
